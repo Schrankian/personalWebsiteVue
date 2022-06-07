333905f21110fb062c294b2a9914a0452ab4b0a8
Schrankian <f.schuster2003@web.de>
2022-06-07 16:07:43 +0200

    custom scrollbar and navbar fix

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,13 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/projects">Projects</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-      <RouterLink to="/contact">Contact</RouterLink>
-    </nav>
-    
-  </header>
-  <div class="capture">
+  <div class="sidebar">
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/projects">Projects</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+    <RouterLink to="/contact">Contact</RouterLink>
+  </div>
+  <div class="maindisplay">
     <RouterView />
   </div>
 </template>
@@ -20,57 +17,49 @@ import { RouterLink, RouterView } from 'vue-router'
 <style>
 @import '@/assets/base.css';
 
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
+.maindisplay{
+ height: 100%;
+ width: 100%;
+ padding-left: 300px;
 }
 
-.capture {
-  width: 60rem;
+.sidebar {
+  height: 100%;
+    width: 300px;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    background-color: var(--color-background);
+    overflow-x: hidden;
+    padding-top: 20px;
 }
 
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: white;
-  transition: 0.4s;
+.sidebar a {
+    padding: 13px 8px 8px 60px;
+    text-decoration: none;
+    font-size: 30px;
+    color: var(--color-text);
+    display: block;
+    transition: 0.3s;
 }
 
 @media (hover: hover) {
   a:hover {
-    background-image: var(--color-gradient);
+    color: orangered;
   }
 }
 
-nav {
-  width: 5%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
+.sidebar a.router-link-exact-active {
   color: orangered;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
 
-nav a {
-  font-size: 2.5rem;
-  display: flex;
-  padding-left: 1rem;
-  padding-right: 10rem;
-}
+
+
+
+
+
 
 @media (min-width: 1024px) {
   body {
@@ -87,12 +76,6 @@ nav a {
   header {
     display: flex;
     place-items: center;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
   }
 
   nav {

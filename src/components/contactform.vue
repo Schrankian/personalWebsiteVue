2cd67b180fed79dc75ade5a1bdb78d4a2e05b31a
Schrankian <f.schuster2003@web.de>
2022-06-10 14:55:13 +0200

    contact form template

---
+++ src/components/contactform.vue
@@ -0,0 +1,16 @@
+<template>
+    <form action="https://formsubmit.co/contact@fabianschuster.net" method="POST">
+        <input type="hidden" name="_subject" value="New submission!">
+        <input type="hidden" name="_template" value="box">
+        <input type="hidden" name="_captcha" value="false">
+        <input type="hidden" name="_next" value="https://www.fabianschuster.net/contact">
+        <input type="text" name="name" placeholder="Enter your Name" required>
+        <input type="email" name="email" placeholder="Enter your Email Adress" required>
+        <textarea name="message" placeholder="Details of your problem"></textarea>
+        <button type="submit">Send</button>
+    </form>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,15 +1,13 @@
+<script setup lang="ts">
+import Contactform from '../components/contactform.vue'
+</script>
+
 <template>
-  <div class="contact">
-    <h1>This is an contact page</h1>
+  <div class="contactform">
+    <Contactform/>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
+
 </style>

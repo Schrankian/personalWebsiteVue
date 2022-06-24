1ea2fe54de9d4b6011a8094b343f76a954e6e3e8
Schrankian <f.schuster2003@web.de>
2022-06-24 12:11:13 +0200

    disabled contact form

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -16,7 +16,8 @@ import { RouterLink, RouterView } from 'vue-router'
       <RouterLink class="app-contact" to="/privacy-policy">Privacy Policy</RouterLink>
     </div>
     <div class="contact-body-item right">
-      <ContactformVue />
+      <!--<ContactformVue />-->
+      <p style="color: rgb(255,100,50);">Contact form is currently unavailable. Please use the email adress to contact me.</p>
     </div>
   </div>
 
